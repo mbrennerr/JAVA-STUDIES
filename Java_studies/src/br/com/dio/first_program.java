bb6c56b5dba308f31fc4d86e2531ca7b9fbb0d1d
mbrennerr <marciobrennerbusiness@gmail.com>
2022-09-10 21:37:42 -0300

    Feat[projects]add_project

---
+++ Java_studies/src/br/com/dio/first_program.java
@@ -0,0 +1,10 @@
+package br.com.dio;
+import br.com.dio.model.Cat;
+
+public class first_program {
+    public static void main(String[] args) {
+        Cat cat = new Cat("Miau", "Black", 2);
+
+        System.out.println(cat);
+    }
+}
